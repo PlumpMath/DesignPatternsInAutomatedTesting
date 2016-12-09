8437f2fdde791b7820aa6b623c373c033831ef87
Edi Prayitno <its.edi@gmail.com>
2016-12-09 15:31:00 +0800

    Page Object Pattern Implementation

---
+++ DesignPatternsInAutomatedTesting/DesignPatternsInAutomatedTesting/PageObjectPattern/Bing/Pages/POP/BingMainPageValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatternsInAutomatedTesting.PageObjectPattern.Bing.Pages.POP
+{
+    public class BingMainPageValidator
+    {
+        private readonly IWebDriver browser;
+
+        public BingMainPageValidator(IWebDriver browser)
+        {
+            this.browser = browser;
+        }
+
+        protected BingMainPageElementMap Map
+        {
+            get
+            {
+                return new BingMainPageElementMap(this.browser);
+            }
+        }
+
+        public void ResultsCount(string expectedCount)
+        {
+            Assert.IsTrue(this.Map.ResultsCountDiv.Text.Contains(expectedCount),
+            "The results DIV doesn't contains the specified text.");
+        }
+    }
+}
